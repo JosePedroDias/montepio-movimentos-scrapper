7a85e1551c06201f797bd54fcf43685974f3ce23
José Pedro Dias <jose.pedro.dias@gmail.com>
2015-11-30 12:14:10 +0000

    improve workflow and readme

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 
+# initial setup
 
-/node_modules/.bin/selenium-standalone start &
+make a copy of the `private-example` to `private`
+edit JSON files with your information
 
-node scrapper.js
+run:
+
+    [sudo] npm install
+    /node_modules/.bin/selenium-standalone install
+
+
+
+# do scrap session
+
+run:
+
+    /node_modules/.bin/selenium-standalone start &
+    node scrapper.js
--- scrapper.js
+++ scrapper.js
@@ -1,6 +1,6 @@
-var credenciais = require('./sensivel/credenciais.json');
-//var matrix = require('./sensivel/cartao-matriz.json');
-//var contas = require('./sensivel/contas.json');
+var credenciais = require('./private/credenciais.json');
+//var matrix      = require('./private/cartao-matriz.json');
+//var contas      = require('./private/contas.json');
 
 var webdriverio = require('webdriverio');
 
@@ -14,22 +14,52 @@ var options = {
 
 var keys = credenciais.password.split('');
 
-webdriverio
+var accountsData;
+var accountToChoose = 'EU';
+var accountValue;
+
+var client = webdriverio
     .remote(options)
-    .init()
+    .init();
+
+client
     .url('http://www.montepio.pt/SitePublico/pt_PT/particulares.page')
     .waitForExist('#loginid_IN', 1000)
     .setValue('#loginid_IN', credenciais.username)
     .click('#net24Submit')
-    .waitForExist('#pinForm input[value="2"]', 1000)
+
+    //.waitForExist('.fldCheckConfirmation', 1000)
     .click('.fldCheckConfirmation')
+
+    .waitForExist('#pinForm input[value="2"]', 2000)
     .click('#pinForm input[value="' + keys[0] + '"]')
     .click('#pinForm input[value="' + keys[1] + '"]')
     .click('#pinForm input[value="' + keys[2] + '"]')
     .click('#pinForm input[value="' + keys[3] + '"]')
     .click('#pinForm input[value="' + keys[4] + '"]')
     .click('#pinForm input[value="' + keys[5] + '"]')
-    .waitForExist('#net24Submit[value="SAIR"]', 1000)
+
+    //.waitForExist('div', 100)
+
     .url('https://net24.montepio.pt/Net24-Web/func/contasordem/ctaOrdemMovimentosCriterios.jsp')
-    .setValue('select[name="seleccaoConta"]', '019100068360|019.10.006836-0 EU||EU')
-    .click('input[name="pesquisaMovimentos"]')
+    //.waitForExist('#net24Submit[value="SAIR"]', 1000)
+    .waitForExist('[name="seleccaoConta"] option', 1000)
+    .selectorExecute('[name="seleccaoConta"] option', function(optEls) {
+        return optEls.map(function(optEl, i) {
+            return {index:i, value:optEl.value, text:optEl.text};
+        });
+    })
+    .then(function(results) {
+        accountsData = results;
+        console.log('accountsData', results);
+        results.forEach(function(r) {
+            if (r.text.indexOf(accountToChoose) !== -1) {
+                accountValue = r.value;
+                console.log('CHOSE ACCOUNT WITH VALUE %s', accountValue);
+            }
+        });
+
+        client
+            .selectByValue('select[name="seleccaoConta"]', accountValue)
+            .click('input[name="pesquisaMovimentos"]');
+    })
